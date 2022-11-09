b7ba932bf622814aa357d104d3f806d1f26a62af
Mithesh <mithesh109@gmail.com>
2022-11-09 20:04:38 +0530

    Implement Edit Product functionality - 127

--- models/product.js
+++ models/product.js
@@ -13,10 +13,11 @@ const getProductsFromFile = cb => {
 }
 
 module.exports = class Product {
-  constructor(title, imageUrl, description, price) {
+  constructor(id, title, imageUrl, description, price) {
     // can handle storing id in constructor or in the save.
     // Max uses handling id in the save method
-    // this.id = Math.floor(Math.random()*20000000000); 
+    // this.id = Math.floor(Math.random()*20000000000);
+    this.id = id;
     this.title = title;
     this.imageUrl = imageUrl;
     this.description = description;
@@ -24,12 +25,21 @@ module.exports = class Product {
   }
 
   save() {
-    this.id = Math.random().toString();
     getProductsFromFile(products => {
-      products.push(this);
-      fs.writeFile(p, JSON.stringify(products), err => {
-        console.log(err);
-      });
+      if (this.id) {
+        const existingProductIndex = products.findIndex(prod => prod.id === this.id);
+        const updatedProducts = [...products];
+        updatedProducts[existingProductIndex] = this;
+        fs.writeFile(p, JSON.stringify(updatedProducts), err => {
+          console.log(err);
+        });
+      } else {
+        this.id = Math.random().toString();
+        products.push(this);
+        fs.writeFile(p, JSON.stringify(products), err => {
+          console.log(err);
+        });
+      }
     });
   }
 
