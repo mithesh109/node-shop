b351e8667026ec4fc574e9f3cbaf832a2030b973
Mithesh <mithesh109@gmail.com>
2022-11-11 10:48:38 +0530

    Use Sequelize to create a product - 152, 153, 154

--- helpers/database.js
+++ helpers/database.js
@@ -1,10 +1,8 @@
-const mysql = require('mysql2');
+const Sequelize = require('sequelize').Sequelize;
 
-const pool = mysql.createPool({
-  host: 'localhost',
-  user: 'root',
-  password: 'password',
-  database: 'node-complete'
+const sequelize = new Sequelize('node-complete', 'root', 'password', {
+  dialect: 'mysql',
+  host: 'localhost'
 });
 
-module.exports = pool.promise();
+module.exports = sequelize;
--- models/product.js
+++ models/product.js
@@ -1,31 +1,30 @@
-const Cart = require('./cart');
-const db = require('../helpers/database');
-module.exports = class Product {
-  constructor(id, title, imageUrl, description, price) {
-    // can handle storing id in constructor or in the save.
-    // Max uses handling id in the save method
-    // this.id = Math.floor(Math.random()*20000000000);
-    this.id = id;
-    this.title = title;
-    this.imageUrl = imageUrl;
-    this.description = description;
-    this.price = price;
-  }
+const Sequelize = require('sequelize');
 
-  save() {
-    return db.execute('INSERT INTO products (title, price, description, imageUrl) VALUES (?, ?, ?, ?)',
-      [this.title, this.price, this.description, this.imageUrl]);
-  }
+const sequelize = require('../helpers/database');
 
-  static deleteById(id) {
-    
+const Product = sequelize.define('product', {
+  id: {
+    type: Sequelize.INTEGER,
+    autoIncrement: true,
+    allowNull: false,
+    primaryKey: true
+  },
+  title: {
+    type: Sequelize.STRING,
+    allowNull: false
+  },
+  price: {
+    type: Sequelize.DOUBLE,
+    allowNull: false
+  },
+  imageUrl: {
+    type: Sequelize.STRING,
+    allowNull: false
+  },
+  description: {
+    type: Sequelize.STRING,
+    allowNull: false
   }
+});
 
-  static fetchAll() {
-    return db.execute('SELECT * FROM products');
-  }
-
-  static findById(id) {
-    return db.execute('SELECT * from products where id = ?', [id]);
-  };
-}
+module.exports = Product;
