dd51cf2eca2eb2e86bbbf227370d3efcc6beb834
Mithesh <mithesh109@gmail.com>
2022-11-17 11:11:54 +0530

    Creating product as admin - 184

--- models/product.js
+++ models/product.js
@@ -1,30 +1,23 @@
-const Sequelize = require('sequelize');
+const getDb = require('../helpers/database').getDb;
 
-const sequelize = require('../helpers/database');
-
-const Product = sequelize.define('product', {
-  id: {
-    type: Sequelize.INTEGER,
-    autoIncrement: true,
-    allowNull: false,
-    primaryKey: true
-  },
-  title: {
-    type: Sequelize.STRING,
-    allowNull: false
-  },
-  price: {
-    type: Sequelize.DOUBLE,
-    allowNull: false
-  },
-  imageUrl: {
-    type: Sequelize.STRING,
-    allowNull: false
-  },
-  description: {
-    type: Sequelize.STRING,
-    allowNull: false
+class Product {
+  constructor(title, price, imageUrl, description) {
+    this.title = title;
+    this.price = price;
+    this.imageUrl = imageUrl;
+    this.description = description;
   }
-});
+
+  save() {
+    const db = getDb();
+    return db.collection('products').insertOne(this)
+    .then(result => {
+      console.log(result);
+    })
+    .catch(err => {
+      console.log(err);
+    });
+  }
+}
 
 module.exports = Product;
